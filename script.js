07afbcdd3cabac9d407f85aca94ba9bd175ac3da
jeremycfwong <71006547+jeremycfwong@users.noreply.github.com>
2022-10-27 09:54:58 +0800

    Add playRound function
    Added playRound function that reads the two input and compute the round
    result.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-var choices = ["Rock", "Paper", "Scissors"]
+var choices = ["rock", "paper", "scissors"]
 
 function getComputerChoice () {
     return choices[Math.floor((Math.random() * 3))];
+}
+
+function playRound(computerSelection, playerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+
+    if ((playerSelection == "rock" && computerSelection == "scissors") 
+    || (playerSelection == "paper" && computerSelection == "rock")
+    || (playerSelection == "scissors" && computerSelection == "paper")){
+        return `Computer selected ${computerSelection}. You win!`
+    } else if (playerSelection == computerSelection) {
+        return `Computer selected ${computerSelection}. It is a draw!`
+    } else {
+        return `Computer selected ${computerSelection}. You Lose!`
+    }
 }
 
